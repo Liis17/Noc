40481b2a0288d233c6dd2a3e949e059a857b796b
Li_is <daskent96@gmail.com>
2020-08-18 22:43:07 +0300

    +1

--- Noc/App.xaml.cs
+++ Noc/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,17 @@ namespace Noc
     /// </summary>
     public partial class App : Application
     {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            foreach (string autoruns in e.Args)
+            {
+                if (autoruns == "тест")
+                {
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+
+        }
     }
 }
--- Noc/MainWindow.xaml.cs
+++ Noc/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,16 @@ namespace Noc
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://ww3.orbitinspace.site/program/noc/elementft");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
         }
     }
 }
